ffa2df136e5c7a0ef725e6812721048fb07e2413
Marcos José <34398442+MarcosJM@users.noreply.github.com>
2018-05-21 15:07:30 -0300

    Add files via upload

--- ControleDados/Login.php
+++ ControleDados/Login.php
@@ -42,7 +42,7 @@
     {
       $idUser= $_POST['userId'];
       $xml = simplexml_load_file( '..\Dados\Usuario.xml');
-      
+
       foreach($xml->usuario as $userSearch)
       {
         $userId= ((String)($userSearch->id));
@@ -57,17 +57,6 @@
         }
       }
 
-      /*$xmlCD= simplexml_load_file( '..\Dados\ComunidadeDiscussao.xml');
-      $iterator= 0;
-      var $discussoes;
-      foreach($xmlCD->comunidadeDiscussao as $cd)
-      {
-        $comunidadeId= $cd->idComunidade;
-        if ($comunidadeId=== '0')
-        {
-
-        }
-      }*/
     }
   }
 
