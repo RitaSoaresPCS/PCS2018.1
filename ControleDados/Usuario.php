8b867854a48b8104ec6ff8e1b45f55e7e5e2c15a
Marcos José <34398442+MarcosJM@users.noreply.github.com>
2018-06-13 10:50:21 -0300

    upload de arquivos alterados
    arquivos php alterados para implementar os casos de uso gerenciar permissão, fixar e desfixar

--- ControleDados/Usuario.php
+++ ControleDados/Usuario.php
@@ -1,158 +1,52 @@
 <?php
-function existeUsernameOuEmail($username, $email) {
-	$xml = simplexml_load_file( '..\Dados\Usuario.xml');
-	foreach($xml->usuario as $us) {
-		$usernameUs = (string)($us->username);
-		$emailUs = (string)($us->email);
-		
-		if (($usernameUs == $username) || 
-			($emailUs == $email)) {
-			return true;
+	header('Content-Type: text/html; charset=utf-8');
+	include_once 'Base.php';
+
+	function verifyAdm($userId) {
+		$sql= "SELECT idUsuarioAdministrador FROM comunidade WHERE idUsuarioAdministrador= $userId";
+		$result= query_result($sql);
+		if (mysqli_num_rows($result) > 0) {
+			return 1;
+		}else{
+			return 0;
 		}
 	}
-}
 
+	function listarUsuariosPermissao(){
+		// ativo é se já entrou no sistema.
+		$sql = "select usuario.id, username, urlImagemPerfil, instituicaoOrigem, usuario.nome, idComunidadePertence, titulo, comunidade.nome as nomeComunidade from usuario, comunidade where usuario.idComunidadePertence = comunidade.id and Usuario.ativo = 1 and Usuario.banidoSistema = 0";
 
-function existeUsernameOuEmailEdicao($username, $email, $id) {
-	$xml = simplexml_load_file( '..\Dados\Usuario.xml');
-	foreach($xml->usuario as $us) {
-		$usernameUs = (string)($us->username);
-		$emailUs = (string)($us->email);
-		$idUs = (string)($us->id);
-		
-		if ((($usernameUs == $username) || ($emailUs == $email)) && ($idUs != $id)) {
-			return true;
-		}
-	}
-}
+		$result = query_result($sql);
+		$return = array();
 
-$func = $_POST['func'];
+		if (mysqli_num_rows($result) > 0) {
 
-switch ($func) {
-    case "adicionar":
-		$id = $_POST['id'];
-		$username = $_POST['username'];
-		$email = $_POST['email'];
-		$senha = $_POST['senha'];
-		$urlImagemPerfil = $_POST['urlImagemPerfil'];
-		$ativo = $_POST['ativo'];
-		$instituicaoOrigem = $_POST['instituicaoOrigem'];
-		$titulo = $_POST['titulo'];
-		$cpf = $_POST['cpf'];
-		$nome = $_POST['nome'];
-		$banidoSistema = $_POST['banidoSistema'];
-		$idComunidadePertence = $_POST['idComunidadePertence'];
-		
-	
-		if( $xml = file_get_contents( '..\Dados\Usuario.xml') ) {
-			$return = array();
-			$return['erro'] = false;
-			
-			$xmldoc = new DomDocument( '1.0' );
-			$xmldoc->preserveWhiteSpace = false;
-			$xmldoc->formatOutput = true;
-
-			$xmldoc->loadXML( $xml, LIBXML_NOBLANKS );
-
-			$root = $xmldoc->getElementsByTagName('Usuario')->item(0);
-			
-			if (existeUsernameOuEmail($username, $email)) {
-				$return['erro'] = true;
-				$return['mensagem'] = "Já existe usuário com este e-mail ou username.";
-				die(json_encode($return));
+			// Para cada linha de resultado:
+			while($row = mysqli_fetch_assoc($result)) {
+				$id = $row["id"];
+				$username = $row["username"];
+				$urlImagemPerfil = $row["urlImagemPerfil"];
+				$instituicaoOrigem = $row["instituicaoOrigem"];
+				$nome = $row["nome"];
+				$idComunidadePertence = $row["idComunidadePertence"];
+				$titulo = $row["titulo"];
+				$nomeComunidade = $row["nomeComunidade"];
+				if ($titulo != 'aluno')
+				{
+					$isAdm= verifyAdm($id);
+				}else{
+					$isAdm= -1;
+				}
+				$s = "{'id': '$id', 'username': '$username', 'urlImagemPerfil': '$urlImagemPerfil', 'instituicaoOrigem': '$instituicaoOrigem', 'nome': '$nome', 'idComunidadePertence': '$idComunidadePertence', 'titulo': '$titulo', 'nomeComunidade': '$nomeComunidade', 'isAdm':'$isAdm' }";
+				array_push($return, str_replace("'", "\"", $s));
 			}
-			
-			$usuario = $xmldoc->createElement('usuario');
-			$root->appendChild($usuario);
-
-
-			$idElement = $xmldoc->createElement('id');
-			$usuario->appendChild($idElement);
-			$idText = $xmldoc->createTextNode($id);
-			$idElement->appendChild($idText);
-			
-			
-			$usernameElement = $xmldoc->createElement('username');
-			$usuario->appendChild($usernameElement);
-			$usernameText = $xmldoc->createTextNode($username);
-			$usernameElement->appendChild($usernameText);
-			
-			
-			$emailElement = $xmldoc->createElement('email');
-			$usuario->appendChild($emailElement);
-			$emailText = $xmldoc->createTextNode($email);
-			$emailElement->appendChild($emailText);
-			
-			
-			$senha = substr(md5(rand()), 0, 7);
-			$senhaElement = $xmldoc->createElement('senha');
-			$usuario->appendChild($senhaElement);
-			$senhaText = $xmldoc->createTextNode(hash('sha256', $senha)); // Senha inicial é uma string aleatória.
-			$senhaElement->appendChild($senhaText);
-			
-			
-			$urlImagemPerfilElement = $xmldoc->createElement('urlImagemPerfil');
-			$usuario->appendChild($urlImagemPerfilElement);
-			$urlImagemPerfilText = $xmldoc->createTextNode($urlImagemPerfil);
-			$urlImagemPerfilElement->appendChild($urlImagemPerfilText);
-
-		
-			$ativoElement = $xmldoc->createElement('ativo');
-			$usuario->appendChild($ativoElement);
-			$ativoText = $xmldoc->createTextNode($ativo);
-			$ativoElement->appendChild($ativoText);
-			
-			
-			$instituicaoOrigemElement = $xmldoc->createElement('instituicaoOrigem');
-			$usuario->appendChild($instituicaoOrigemElement);
-			$instituicaoOrigemText = $xmldoc->createTextNode($instituicaoOrigem);
-			$instituicaoOrigemElement->appendChild($instituicaoOrigemText);
-		
-
-			$tituloElement = $xmldoc->createElement('titulo');
-			$usuario->appendChild($tituloElement);
-			$tituloText = $xmldoc->createTextNode($titulo);
-			$tituloElement->appendChild($tituloText);
-			
-			
-			$cpfElement = $xmldoc->createElement('cpf');
-			$usuario->appendChild($cpfElement);
-			$cpfText = $xmldoc->createTextNode($cpf);
-			$cpfElement->appendChild($cpfText);
-
-			$nomeElement = $xmldoc->createElement('nome');
-			$usuario->appendChild($nomeElement);
-			$nomeText = $xmldoc->createTextNode($nome);
-			$nomeElement->appendChild($nomeText);
-			
-			$banidoSistemaElement = $xmldoc->createElement('banidoSistema');
-			$usuario->appendChild($banidoSistemaElement);
-			$banidoSistemaText = $xmldoc->createTextNode($banidoSistema);
-			$banidoSistemaElement->appendChild($banidoSistemaText);
-		
-			
-			$idComunidadePertenceElement = $xmldoc->createElement('idComunidadePertence');
-			$usuario->appendChild($idComunidadePertenceElement);
-			$idComunidadePertenceText = $xmldoc->createTextNode($idComunidadePertence);
-			$idComunidadePertenceElement->appendChild($idComunidadePertenceText);
-		
-
-			
-			// Envio de email para o novo usuário cadastrado, com a $senha e $username.
-			// Valida o e-mail primeiro, depois tenta enviar. Portanto, aqui pode retornar dois erros possíveis:
-			//$return['erro'] = true;
-			//$return['mensagem'] = "E-mail inválido."; ou $return['mensagem'] = "Não foi possível enviar e-mail, usuário não criado";
-			// Somente salva o arquivo se o e-mail for enviado com sucesso.
-			$xmldoc->save('..\Dados\Usuario.xml');
-			
-			die(json_encode($return));
-			
 		}
-		break;
-		
-		
-	case "editar":
-		$id = $_POST['id'];
+
+		// Retorna o array concatenado.
+		echo "[" . implode(",", $return) . "]";
+	}
+
+	function adicionarUsuario() {
 		$username = $_POST['username'];
 		$email = $_POST['email'];
 		$instituicaoOrigem = $_POST['instituicaoOrigem'];
@@ -160,53 +54,268 @@ switch ($func) {
 		$cpf = $_POST['cpf'];
 		$nome = $_POST['nome'];
 		$idComunidadePertence = $_POST['idComunidadePertence'];
-	
+
 		$return = array();
 		$return['erro'] = false;
-		
-		if (existeUsernameOuEmailEdicao($username, $email, $id)) {
+
+		if (existeUsernameOuEmail($username, $email)) {
 			$return['erro'] = true;
 			$return['mensagem'] = "Já existe usuário com este e-mail ou username.";
-			die(json_encode($return));
+			echo json_encode($return);
+			return;
 		}
 
-		$xml = simplexml_load_file( '..\Dados\Usuario.xml');
-		
-		foreach($xml->usuario as $us) {
-			$idUs = (string)($us->id);
-			
-			if ($idUs == $id) {				
-				$us->username = $username;
-				$us->email = $email;
-				$us->instituicaoOrigem = $instituicaoOrigem;
-				$us->titulo = $titulo;
-				$us->cpf = $cpf;
-				$us->nome = $nome;
-				$us->idComunidadePertence = $idComunidadePertence;
-				break;
+		$senha = substr(md5(rand()), 0, 7);
+		$senhaHash = hash('sha256', $senha); // Senha inicial é uma string aleatória.
+
+		// Envio de email para o novo usuário cadastrado, com a $senha e $username.
+		// Valida o e-mail primeiro, depois tenta enviar. Portanto, aqui pode retornar dois erros possíveis:
+		//$return['erro'] = true;
+		//$return['mensagem'] = "E-mail inválido."; ou $return['mensagem'] = "Não foi possível enviar e-mail, usuário não criado";
+		// Somente salva o arquivo se o e-mail for enviado com sucesso.
+
+		$sql = "INSERT INTO Usuario VALUES (default, '$username', '$email', '$senhaHash', '', 1, '$instituicaoOrigem', '$titulo', '$cpf', '$nome', 0, $idComunidadePertence)";
+
+		// Retorna um json com o resultado.
+		echo query_no_result($sql);
+	}
+
+
+	function listarUsuario() {
+		// ativo é se já entrou no sistema.
+		$sql = "select usuario.id, username, urlImagemPerfil, instituicaoOrigem, usuario.nome, idComunidadePertence, titulo, comunidade.nome as nomeComunidade from usuario, comunidade where usuario.idComunidadePertence = comunidade.id and Usuario.ativo = 1 and Usuario.banidoSistema = 0";
+
+		$result = query_result($sql);
+		$return = array();
+
+		if (mysqli_num_rows($result) > 0) {
+
+			// Para cada linha de resultado:
+			while($row = mysqli_fetch_assoc($result)) {
+				$id = $row["id"];
+				$username = $row["username"];
+				$urlImagemPerfil = $row["urlImagemPerfil"];
+				$instituicaoOrigem = $row["instituicaoOrigem"];
+				$nome = $row["nome"];
+				$idComunidadePertence = $row["idComunidadePertence"];
+				$titulo = $row["titulo"];
+				$nomeComunidade = $row["nomeComunidade"];
+
+				$s = "{'id': '$id', 'username': '$username', 'urlImagemPerfil': '$urlImagemPerfil', 'instituicaoOrigem': '$instituicaoOrigem', 'nome': '$nome', 'idComunidadePertence': '$idComunidadePertence', 'titulo': '$titulo', 'nomeComunidade': '$nomeComunidade' }";
+				array_push($return, str_replace("'", "\"", $s));
 			}
 		}
-		$xml->asXml('..\Dados\Usuario.xml'); // Salva.
-		die(json_encode($return));
-		break;
-		
-	case "inativar":
+
+		// Retorna o array concatenado.
+		echo "[" . implode(",", $return) . "]";
+	}
+
+
+	function getByIdUsuario() {
 		$id = $_POST['id'];
-	
-		$xml = simplexml_load_file( '..\Dados\Usuario.xml');
-		foreach($xml->usuario as $us) {
-			$idUs = (string)($us->id);
-			
-			if ($idUs == $id) {
-				$us->ativo = "false";
-				$us->banidoSistema = "true";
-				break;
+		$sql = "select usuario.id, username, urlImagemPerfil, instituicaoOrigem, usuario.nome, idComunidadePertence, titulo, comunidade.nome as nomeComunidade, email, cpf FROM usuario, comunidade where usuario.idComunidadePertence = comunidade.id and usuario.id = $id";
+
+		$result = query_result($sql);
+		$return = array();
+
+		if (mysqli_num_rows($result) > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+				$id = $row["id"];
+				$username = $row["username"];
+				$urlImagemPerfil = $row["urlImagemPerfil"];
+				$instituicaoOrigem = $row["instituicaoOrigem"];
+				$nome = $row["nome"];
+				$idComunidadePertence = $row["idComunidadePertence"];
+				$titulo = $row["titulo"];
+				$nomeComunidade = $row["nomeComunidade"];
+				$email = $row["email"];
+				$cpf = $row["cpf"];
+
+				$s = "{'id': '$id', 'username': '$username', 'urlImagemPerfil': '$urlImagemPerfil', 'instituicaoOrigem': '$instituicaoOrigem', 'nome': '$nome', 'idComunidadePertence': '$idComunidadePertence', 'titulo': '$titulo', 'nomeComunidade': '$nomeComunidade', 'email': '$email', 'cpf': '$cpf' }";
+
+				array_push($return, str_replace("'", "\"", $s));
 			}
 		}
-		
-		$xml->asXml('..\Dados\Usuario.xml'); // Salva.
+		echo json_encode($return);
+	}
 
-		break;
-}
 
+	function getByNomeUsuario() {
+		$nome = $_POST['nome'];
+
+		$sql = "SELECT * FROM Usuario WHERE LOWER(nome) LIKE LOWER('%$nome%')";
+
+		$result = query_result($sql);
+		$return = array();
+
+		if (mysqli_num_rows($result) > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+				$id = $row["id"];
+				$username = $row["username"];
+				$urlImagemPerfil = $row["urlImagemPerfil"];
+				$instituicaoOrigem = $row["instituicaoOrigem"];
+				$nome = $row["nome"];
+				$idComunidadePertence = $row["idComunidadePertence"];
+
+				$s = "{'id': '$id', 'username': '$username', 'urlImagemPerfil': '$urlImagemPerfil', 'instituicaoOrigem': '$instituicaoOrigem', 'nome': '$nome', 'idComunidadePertence': '$idComunidadePertence'}";
+				array_push($return, str_replace("'", "\"", $s));
+			}
+		}
+		echo "[" . implode(",", $return) . "]";
+	}
+
+
+	function getByNomeIgualUsuario() {
+		$nome = $_POST['nome'];
+
+		$sql = "SELECT * FROM Usuario WHERE LOWER(nome) = LOWER('$nome')";
+
+		$result = query_result($sql);
+		$return = array();
+
+		if (mysqli_num_rows($result) > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+				$id = $row["id"];
+				$username = $row["username"];
+				$urlImagemPerfil = $row["urlImagemPerfil"];
+				$instituicaoOrigem = $row["instituicaoOrigem"];
+				$nome = $row["nome"];
+				$idComunidadePertence = $row["idComunidadePertence"];
+
+				$s = "{'id': '$id', 'username': '$username', 'urlImagemPerfil': '$urlImagemPerfil', 'instituicaoOrigem': '$instituicaoOrigem', 'nome': '$nome', 'idComunidadePertence': '$idComunidadePertence'}";
+				array_push($return, str_replace("'", "\"", $s));
+			}
+		}
+		echo "[" . implode(",", $return) . "]";
+	}
+
+
+	function getByUsernameUsuario() {
+		$username = $_POST['username'];
+
+		$sql = "SELECT * FROM Usuario WHERE LOWER(username) = LOWER('$username')";
+
+		$result = query_result($sql);
+		$return = array();
+
+		if (mysqli_num_rows($result) > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+				$id = $row["id"];
+				$username = $row["username"];
+				$urlImagemPerfil = $row["urlImagemPerfil"];
+				$instituicaoOrigem = $row["instituicaoOrigem"];
+				$nome = $row["nome"];
+				$idComunidadePertence = $row["idComunidadePertence"];
+
+				$s = "{'id': '$id', 'username': '$username', 'urlImagemPerfil': '$urlImagemPerfil', 'instituicaoOrigem': '$instituicaoOrigem', 'nome': '$nome', 'idComunidadePertence': '$idComunidadePertence'}";
+				array_push($return, str_replace("'", "\"", $s));
+			}
+		}
+		echo json_encode($return);
+	}
+
+
+	function getIdUsuarioByUsername($username) {
+		$sql = "SELECT id FROM Usuario WHERE LOWER(username) = LOWER('$username')";
+
+		$result = query_result($sql);
+
+		if (mysqli_num_rows($result) > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+				return $row["id"];
+			}
+		}
+		return "";
+	}
+
+
+	function editarUsuario() {
+		$id = $_POST['id'];
+		$username = $_POST['username'];
+		$email = $_POST['email'];
+		$instituicaoOrigem = $_POST['instituicaoOrigem'];
+		$titulo = $_POST['titulo'];
+		$cpf = $_POST['cpf'];
+		$nome = $_POST['nome'];
+		$idComunidadePertence = $_POST['idComunidadePertence'];
+
+		$return = array();
+		$return['erro'] = false;
+
+		if (existeUsernameOuEmail($username, $email, $id)) {
+			$return['erro'] = true;
+			$return['mensagem'] = "Já existe usuário com este e-mail ou username.";
+			echo json_encode($return);
+			return;
+		}
+
+		$sql = "UPDATE Usuario SET username = '$username', email = '$email', instituicaoOrigem = '$instituicaoOrigem', titulo = '$titulo', cpf = '$cpf', nome = '$nome', idComunidadePertence = '$idComunidadePertence' WHERE id = $id";
+
+		echo query_no_result($sql);
+	}
+
+
+	function inativarUsuario() {
+		$id = $_POST['id'];
+		// colocar ativo = 0 ?
+		$sql = "UPDATE Usuario SET banidoSistema = 1 WHERE id = $id";
+		echo query_no_result($sql);
+	}
+
+
+	function existeEmail($email) {
+		$sql = "SELECT * FROM Usuario WHERE email='$email'";
+
+		$result = query_result($sql);
+
+		if (mysqli_num_rows($result) > 0) {
+			return true;
+		}
+		return false;
+	}
+
+
+	function existeUsernameOuEmail($username, $email, $id = -1) {
+		$sql = "SELECT * FROM Usuario WHERE (username='$username' or email='$email') and id != $id";
+
+		$result = query_result($sql);
+
+		if (mysqli_num_rows($result) > 0) {
+			return true;
+		}
+		return false;
+	}
+
+
+	$func = $_POST['func'];
+
+	switch ($func) {
+		case "adicionar":
+			adicionarUsuario();
+			break;
+		case "listar":
+			listarUsuario();
+			break;
+		case "getById":
+			getByIdUsuario();
+			break;
+		case "getByNome":
+			getByNomeUsuario();
+			break;
+		case "getByNomeIgual":
+			getByNomeIgualUsuario();
+			break;
+		case "getByUsername":
+			getByUsernameUsuario();
+			break;
+		case "editar":
+			editarUsuario();
+			break;
+		case "inativar":
+			inativarUsuario();
+			break;
+		case "listarUsuariosPermissao":
+				listarUsuariosPermissao();
+			break;
+	}
 ?>
