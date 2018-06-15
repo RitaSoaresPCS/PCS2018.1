be6c6c96f3a5942ae51f7f3579110fe0e25cee89
RitaMenezes <38992717+RitaSoaresPCS@users.noreply.github.com>
2018-06-15 16:29:33 -0300

    correção em discussão

--- AcessoDados/Discussao.js
+++ AcessoDados/Discussao.js
@@ -44,13 +44,14 @@ var Discussao = new function() {
 	}
 
 
-	this.adicionar = function (idComunidadePertence, titulo, descricao, publica, callback = function(data) {}) {
+	this.adicionar = function (idComunidadePertence, titulo, descricao, publica, userId, callback = function(data) {}) {
 		$.post(this.controladorURL, {
 			func: "criar",
 			idComunidadePertence: idComunidadePertence,
 			titulo: titulo,
 			descricao: descricao,
-			publica: publica
+			publica: publica,
+			userId: userId
 			},
 			function(data) {
 				callback(data);
@@ -60,14 +61,13 @@ var Discussao = new function() {
 	}
 
 
-	this.editar = function (id, titulo, descricao, publica, userId, callback = function(data) {}) {
+	this.editar = function (id, titulo, descricao, publica, callback = function(data) {}) {
 		$.post(this.controladorURL, {
 			func: "editar",
 			id: id,
 			titulo: titulo,
 			descricao: descricao,
-			publica: publica,
-			userId: userId
+			publica: publica
 			},
 			function(data) {
 				callback(data);
